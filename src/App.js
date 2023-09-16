102b4e94cb8e54b74667550098f823468e45ca7d
Devaiah Coluvanda Bopanna <devaiah.coluvandabopanna@nagra.com>
2023-09-16 13:17:18 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import  Header  from "./MyComponents/Header";
+import React from 'react';
+import Footer from "./MyComponents/Footer";
+import Todos  from "./MyComponents/Todos";
+// import Todo from "./MyComponents/Todo";
+import { useState } from "react";
+
 
 function App() {
+  let OnDelete = (todo)=>
+{
+console.log("Todo is deleted" , todo)
+SetList(list.filter((e)=>
+{
+return e!==todo;
+}
+))
+}
+const [list, SetList] = useState([
+  {
+    slno: 1,
+    title: "Go to the market",
+    desc: "Buy fruits and vegetables"
+  },
+  {
+    slno: 2,
+    title: "Go to the mall",
+    desc: "Buy black shirt"
+  },
+  {
+    slno: 3,
+    title: "Go to the bike mechanic",
+    desc: "Bike wash and chain lube"
+  }
+]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Header title="MY TODOS LIST"/>
+     <Todos list={list} OnDelete={OnDelete}/>
+     {/* <Todo/> */}
+     <Footer/>
+     </>
   );
 }
-
 export default App;
